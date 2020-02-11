56bbddeec2fdc9c2add1bc25459ac3c7396e3ec3
yangwenqiang.ywq <yangwenqiang.ywq@bytedance.com>
2020-02-11 22:58:23 +0800

    add gevent dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-version = '0.2.2'
+version = '0.2.3'
 
 entry_points = {
     'console_scripts': [
@@ -23,6 +23,7 @@ setup(
         'Click',
         'tqdm',
         'requests',
+        'gevent',
     ],
     license="MIT",
     entry_points=entry_points,
